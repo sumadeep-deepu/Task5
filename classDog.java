5ed259e20eb15a9b9973d542f235d34b8944150f
sumadeep-deepu <147999349+sumadeep-deepu@users.noreply.github.com>
2023-10-17 11:55:41 +0530

    Add files via upload

---
+++ classDog.java
@@ -0,0 +1,21 @@
+public class classDog {
+    public String name="sheero";
+    public int height= 10;
+    public int weight=20;
+    public static void main(String[] args) {
+        
+        classDog dog= new classDog();
+        classDog cat =new classDog();
+        
+        System.out.println(dog.name);
+
+        System.out.println(dog.weight);
+        
+        System.out.println(dog.height);
+
+
+        System.out.println(cat.height);
+        
+    }
+    
+}
---
+++ constructors.java
@@ -0,0 +1,17 @@
+public class constructors {
+    int i;
+    public static void main(String[] args) {
+        constructors cns=new constructors(26);
+        System.out.println(cns.i);
+    }
+          public constructors(){
+            i=20;
+    }
+        public constructors(int b){
+            i=b;
+
+        }
+        
+
+    
+}
